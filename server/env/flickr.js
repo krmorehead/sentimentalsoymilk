a0f1c8e6c4cd93c4d77afb4c50793041c1c8df75
Kyle Morehead <krmorehead@gmail.com>
2016-01-13 19:39:28 -0800

    commented out flickr section to avoid flooding your console

--- server/env/flickr.js
+++ server/env/flickr.js
@@ -1,51 +1,51 @@
 var Flickr = require('flickr').Flickr;
 
-var flickrOptions = {
-  api_key: "d04e5aac7cf3921706e138faacd9f276",
-  secret: "7e1000af685d527d"
-};
+// var flickrOptions = {
+//   api_key: "d04e5aac7cf3921706e138faacd9f276",
+//   secret: "7e1000af685d527d"
+// };
 
-var flickr = new Flickr('d04e5aac7cf3921706e138faacd9f276', '7e1000af685d527d')
-var flickr_params = {
-  per_page: 25,
-  page: 1,
-  geo_context: 2,
-}
+// var flickr = new Flickr('d04e5aac7cf3921706e138faacd9f276', '7e1000af685d527d')
+// var flickr_params = {
+//   per_page: 25,
+//   page: 1,
+//   geo_context: 2,
+// }
 
-//adds an array of tags to the params
-flickr.addTags = function(tags){
-	flickr_params["tags"] = tags.join(",")
-}
+// //adds an array of tags to the params
+// flickr.addTags = function(tags){
+// 	flickr_params["tags"] = tags.join(",")
+// }
 
-flickr.addGeo = function(lat, lon){
-	flickr_params["lat"] = lat;
-	flickr_params["lon"] = lon;
-	flickr_params["accuracy"] = 11;
-}
+// flickr.addGeo = function(lat, lon){
+// 	flickr_params["lat"] = lat;
+// 	flickr_params["lon"] = lon;
+// 	flickr_params["accuracy"] = 11;
+// }
 
-//builds a image url from the flickr object returned
-flickr.formatUrl = function(photoObj, size){
-	// farmId, serverId, userId, secret
-	var url = "https://farm" + photoObj.farm
-	+ ".staticflickr.com/" + photoObj.server
-	+ "/" + photoObj.id
-	+ "_" + photoObj.secret + ".jpg"
-	if(size){
-		url += size
-	}
-	return url
-}
+// //builds a image url from the flickr object returned
+// flickr.formatUrl = function(photoObj, size){
+// 	// farmId, serverId, userId, secret
+// 	var url = "https://farm" + photoObj.farm
+// 	+ ".staticflickr.com/" + photoObj.server
+// 	+ "/" + photoObj.id
+// 	+ "_" + photoObj.secret + ".jpg"
+// 	if(size){
+// 		url += size
+// 	}
+// 	return url
+// }
 
-flickr.executeAPIRequest("flickr.photos.search", flickr_params, false, function(err, result) {
-  // Show the error if we got one
-  if(err) {
-      console.log("FLICKR ERROR: " + err);
+// flickr.executeAPIRequest("flickr.photos.search", flickr_params, false, function(err, result) {
+//   // Show the error if we got one
+//   if(err) {
+//       console.log("FLICKR ERROR: " + err);
 
-      return;
-  }else{
-  	console.log(result.photos)
-  }
-});
+//       return;
+//   }else{
+//   	console.log(result.photos)
+//   }
+// });
 
 //formula for making a link to image
 //
