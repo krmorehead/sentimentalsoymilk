4b924eb4d686baf0faa44bf7a3142d837b281bb3
Kartik <kvempati11@gmail.com>
2016-01-15 15:35:50 -0800

    Include html templating for a drop down menu. injection error

--- public/js/controllers/createTripController.js
+++ public/js/controllers/createTripController.js
@@ -133,10 +133,6 @@ angular.module('app.create', ['app.services'])
     $scope.popup1.opened = true;
   };
 
-  $scope.open2 = function() {
-    $scope.popup2.opened = true;
-  };
-
   $scope.setDate = function(year, month, day) {
     $scope.dt = new Date(year, month, day);
     console.log($scope.dt);
@@ -155,10 +151,6 @@ angular.module('app.create', ['app.services'])
     opened: false
   };
 
-  $scope.popup2 = {
-    opened: false
-  };
-
   var tomorrow = new Date();
   tomorrow.setDate(tomorrow.getDate() + 1);
   var afterTomorrow = new Date();
@@ -174,6 +166,17 @@ angular.module('app.create', ['app.services'])
         status: 'partially'
       }
     ];
+
+  $scope.categories = ["Arts & Entertainment",
+                      "Landmarks",
+                      "Nightlife",
+                      "Restaurants",
+                      "Bars",
+                      "Coffee & Tea",
+                      "Delivery",
+                      "Tourist",
+                      "Shopping",
+                      "Beauty & Spas"];
 
   $scope.getDayClass = function(date, mode) {
     if (mode === 'day') {
